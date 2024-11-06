1f5940a97e1d1f1ab0eb2bb25bf91e9ff411dc0f
Panos Zafeiropoulos <zzpzaf@gmail.com>
2024-11-06 08:50:00 +0100

    A new fake/dummy component (NoopComponent) with a router path in app.routes.ts was added. ({ path: '**', component: FakeComponent }). The Location service has been injected and initially used in the ContentService to capture the URL path.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { NoopComponent } from './noop.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '**', component: NoopComponent } // Fallback for unmatched routes
+    // { path: '**', redirectTo: '/', pathMatch: 'full' } // Redirects all unmatched paths to the root
+];
--- src/app/content.service.ts
+++ src/app/content.service.ts
@@ -1,31 +1,63 @@
 import { inject, Injectable, signal } from '@angular/core';
 import { DataService } from './data.service';
 import { IArticle, ICategory, Pages } from './dbObjects/blogObjects';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+import { Location } from '@angular/common';
 
 const ComponentName = 'ContentService';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContentService {
-  constructor() { 
+  constructor() {
     if (this.$categories.length === 0) this.signalCategories();
     if (this.$categoryArticles().length === 0) this.signalCategoryArticles(1);
+
+    // this.router.events
+    //   .pipe(filter((event) => event instanceof NavigationEnd))
+    //   .subscribe(() => {
+    //     this.currentPath = this.router.url; // Updates path on navigation changes
+    //     console.log('>===>> ' + ComponentName + ' - ' +"Browser's URL path: " + this.currentPath);
+    //   });
+
+    // Capture the current browser's URL path
+    this.currentPath = this.location.path();
+    console.log('>===>> ' + ComponentName + ' - ' + "Initial path: " + this.currentPath);
+
+    // Subscribe to URL changes reactively
+    this.location.onUrlChange((url: string) => {
+      this.currentPath = url;
+      console.log("URL changed to: " + this.currentPath);
+      console.log('>===>> ' + ComponentName + ' - ' + "URL path: changed to:" + this.currentPath);
+    });
+
+
   }
 
   private dataService = inject(DataService);
+  // private router = inject(Router);
+  // private activeRoute = inject(ActivatedRoute);
+  private location = inject(Location);
 
   componentName = this.constructor.name.replace('_', '');
 
   public $noPostsPageNr = signal<number>(0);
   public $pageContent = signal<string>('');
 
-  public $categories = signal< ICategory[]>([]); 
-  public $category = signal< ICategory>({categoryId: 0, categoryTitle: ''});
-  public $article = signal< IArticle>({articleId: 0, categoryId: 0, articleTitle: '', articleSubTitle: '', articleContent:  ''});
-  public $categoryArticles = signal< IArticle[]>([]); 
+  private currentPath: string | undefined;
 
-
+  public $categories = signal<ICategory[]>([]);
+  public $category = signal<ICategory>({ categoryId: 0, categoryTitle: '' });
+  public $article = signal<IArticle>({
+    articleId: 0,
+    categoryId: 0,
+    articleTitle: '',
+    articleSubTitle: '',
+    articleContent: '',
+  });
+  public $categoryArticles = signal<IArticle[]>([]);
 
   public signalCategories(): void {
     this.dataService.getCategories().subscribe((categories: ICategory[]) => {
@@ -34,32 +66,45 @@ export class ContentService {
     });
   }
 
-  public signalCategory(categoryId:number): void {
-    this.dataService.getCategory(categoryId).subscribe((category: ICategory) => {
-      if (category) {
-        this.$category.set(category);
-        this.signalCategoryArticles( this.$category().categoryId);
-      } else {
-        this.$category.set({categoryId: 0, categoryTitle: "Category Not Found!"});
-      }
-    });   
+  public signalCategory(categoryId: number): void {
+    this.dataService
+      .getCategory(categoryId)
+      .subscribe((category: ICategory) => {
+        if (category) {
+          this.$category.set(category);
+          this.signalCategoryArticles(this.$category().categoryId);
+        } else {
+          this.$category.set({
+            categoryId: 0,
+            categoryTitle: 'Category Not Found!',
+          });
+        }
+      });
   }
 
   public signalCategoryArticles(categoryId: number): void {
-    this.dataService.getCategoryArticles(categoryId).subscribe((categoryarticles: IArticle[]) => {
-      this.$categoryArticles.set(categoryarticles);
-      this.signalArticle(this.$categoryArticles()[0].articleId);
-    });
+    this.dataService
+      .getCategoryArticles(categoryId)
+      .subscribe((categoryarticles: IArticle[]) => {
+        this.$categoryArticles.set(categoryarticles);
+        this.signalArticle(this.$categoryArticles()[0].articleId);
+      });
   }
 
-  public signalArticle(articleId:number): void {
+  public signalArticle(articleId: number): void {
     this.dataService.getArticle(articleId).subscribe((article: IArticle) => {
       if (article) {
         this.$article.set(article);
       } else {
-        this.$article.set({articleId: 0, categoryId: 0, articleTitle: "Not Found!", articleSubTitle: "Article Not Found!", articleContent: "Not Found!"});
+        this.$article.set({
+          articleId: 0,
+          categoryId: 0,
+          articleTitle: 'Not Found!',
+          articleSubTitle: 'Article Not Found!',
+          articleContent: 'Not Found!',
+        });
       }
-    });   
+    });
   }
 
   public signalPageContent(pageId: number): void {
@@ -96,8 +141,7 @@ export class ContentService {
         });
     } else {
       this.$pageContent.set('Unknown HTML Page!');
-    }  
+    }
     // console.log('>===>> ' + ComponentName + ' - ' + 'Page Id: ' + this.$noPostsPageNr() + ' Content: ' + this.$pageContent());
   }
-
 }
